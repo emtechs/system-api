434d9a6084fa425e7a4c85de6a5aaf04507a27ce
Lucas <lucassoares2007@hotmail.com>
2023-06-12 03:35:01 -0300

    refactor: list server

--- src/services/users/listServer.service.ts
+++ src/services/users/listServer.service.ts
@@ -4,13 +4,14 @@ import { ServerArraySchema } from '../../schemas';
 
 export const listServerService = async (
   server_id: string,
-  { name, take, skip, order, by }: ISchoolQuery,
+  { is_active, name, take, skip, order, by }: ISchoolQuery,
 ) => {
   if (take) take = +take;
   if (skip) skip = +skip;
 
   let whereData = {};
   let orderBy = {};
+  let whereSchool = {};
 
   if (order) {
     switch (order) {
@@ -20,21 +21,60 @@ export const listServerService = async (
     }
   }
 
-  if (name)
+  if (is_active) {
+    switch (is_active) {
+    case 'true':
+      whereSchool = {
+        ...whereSchool,
+        is_active: true,
+      };
+      whereData = {
+        ...whereData,
+        school: {
+          ...whereSchool,
+        },
+      };
+      break;
+
+    case 'false':
+      whereSchool = {
+        ...whereSchool,
+        is_active: false,
+      };
+      whereData = {
+        ...whereData,
+        school: {
+          ...whereSchool,
+        },
+      };
+      break;
+    }
+  }
+
+  if (name) {
+    whereSchool = {
+      ...whereSchool,
+      name: { contains: name, mode: 'insensitive' },
+    };
     whereData = {
       ...whereData,
       school: {
-        name: { contains: name, mode: 'insensitive' },
-        is_active: true,
+        ...whereSchool,
       },
     };
+  }
 
-  whereData = { ...whereData, server_id };
+  whereData = {
+    ...whereData,
+    server_id,
+  };
 
   const servers = await prisma.schoolServer.findMany({
     take,
     skip,
-    where: { ...whereData },
+    where: {
+      ...whereData,
+    },
     include: { school: true },
     orderBy,
   });
