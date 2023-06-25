2a3d89515e27c020e97d1160dd297634e0d184b2
Lucas <lucassoares2007@hotmail.com>
2023-06-25 20:21:23 -0300

    refactor: list work school

--- src/services/users/listWorkSchool.service.ts
+++ src/services/users/listWorkSchool.service.ts
@@ -8,20 +8,27 @@ export const listWorkSchoolService = async (
   if (take) take = +take;
   if (skip) skip = +skip;
 
+  const where = { server_id, school: { is_active: true } };
+  const school = { select: { id: true, name: true } };
+
   const [workSchools, total, schoolsData] = await Promise.all([
     prisma.schoolServer.findMany({
       take,
       skip,
-      where: { server_id },
-      select: { role: true, dash: true, school: true },
+      where,
+      select: {
+        role: true,
+        dash: true,
+        school,
+      },
       orderBy: { school: { name: 'asc' } },
     }),
     prisma.schoolServer.count({
-      where: { server_id },
+      where,
     }),
     prisma.schoolServer.findMany({
-      where: { server_id },
-      select: { school: true },
+      where,
+      select: { school },
       orderBy: { school: { name: 'asc' } },
     }),
   ]);
