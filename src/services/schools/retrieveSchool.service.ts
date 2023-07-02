13114cb3c2799305467d5068995e15c596f74568
Lucas <lucassoares2007@hotmail.com>
2023-07-02 07:27:32 -0300

    refactor: retrieve school

--- src/services/schools/retrieveSchool.service.ts
+++ src/services/schools/retrieveSchool.service.ts
@@ -1,5 +1,5 @@
 import prisma from '../../prisma';
-import { SchoolReturnSchema } from '../../schemas';
+import { SchoolReturnSchema, ServerArraySchema } from '../../schemas';
 
 export const retrieveSchoolService = async (id: string) => {
   const select = {
@@ -9,10 +9,27 @@ export const retrieveSchoolService = async (id: string) => {
     director: { select: { id: true, cpf: true, name: true } },
   };
 
-  const school = await prisma.school.findUnique({
-    where: { id },
-    select,
-  });
+  const [school, servers, total, years] = await Promise.all([
+    prisma.school.findUnique({
+      where: { id },
+      select,
+    }),
+    prisma.schoolServer.findMany({
+      where: { school_id: id },
+      select: {
+        role: true,
+        dash: true,
+        server: { select: { id: true, name: true, cpf: true } },
+      },
+      orderBy: { server: { name: 'asc' } },
+    }),
+    prisma.schoolServer.count({ where: { school_id: id } }),
+    prisma.year.findMany({ where: { classes: { some: { school_id: id } } } }),
+  ]);
 
-  return SchoolReturnSchema.parse(school);
+  return {
+    school: SchoolReturnSchema.parse(school),
+    servers: { total, result: ServerArraySchema.parse(servers) },
+    years,
+  };
 };
