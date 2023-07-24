2a09cc80d044cfdb794c23f4094e92cf55d96013
Lucas <lucassoares2007@hotmail.com>
2023-07-24 01:12:14 -0300

    feat: refesh token

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { handleError } from './http/error'
+import { env } from './env'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.SECRET_KEY,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
 
 app.setErrorHandler(handleError)
