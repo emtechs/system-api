df9e0a82c7cf71c82738c3286f068a681de77633
Lucas <lucassoares2007@hotmail.com>
2023-07-27 01:12:26 -0300

    refactor: list student

--- src/services/students/listStudent.service.ts
+++ src/services/students/listStudent.service.ts
@@ -1,72 +1,36 @@
 import { IStudentQuery } from '../../interfaces'
 import { prisma } from '../../lib'
-import { studentArrayReturn } from '../../scripts'
 
 export const listStudentService = async ({
-  year_id,
-  school_id,
-  class_id,
   take,
   skip,
-  by,
-  order,
   name,
 }: IStudentQuery) => {
   if (take) take = +take
   if (skip) skip = +skip
 
   let where = {}
-  let where_classes = {}
-  let orderBy = {}
 
-  if (order) {
-    switch (order) {
-      case 'name':
-        orderBy = { name: by }
-        break
-
-      case 'registry':
-        orderBy = [{ registry: by }, { name: 'asc' }]
-        break
+  if (name)
+    where = {
+      OR: [
+        { name: { contains: name, mode: 'insensitive' } },
+        { registry: { contains: name, mode: 'insensitive' } },
+      ],
     }
-  }
-
-  if (year_id) where_classes = { ...where_classes, some: { year_id } }
-
-  if (school_id) where_classes = { ...where_classes, some: { school_id } }
-
-  if (class_id) where_classes = { ...where_classes, some: { class_id } }
-
-  if (year_id && school_id && class_id)
-    where_classes = {
-      every: {
-        class_id,
-        school_id,
-        year_id,
-        is_active: true,
-      },
-    }
-
-  if (name) where = { ...where, name: { contains: name, mode: 'insensitive' } }
-
-  where = {
-    ...where,
-    classes: { ...where_classes },
-    NOT: { classes: { none: {} } },
-  }
 
   const [students, total] = await Promise.all([
     prisma.student.findMany({
       take,
       skip,
       where,
-      orderBy,
+      orderBy: { name: 'asc' },
     }),
     prisma.student.count({ where }),
   ])
 
   return {
     total,
-    result: await studentArrayReturn(students, year_id),
+    result: students,
   }
 }
