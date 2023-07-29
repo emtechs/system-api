62b0e3b42f88a97c7952e7de3c1b968069b1bb24
Lucas <lucassoares2007@hotmail.com>
2023-07-29 00:33:33 -0300

    refactor: infrequency

--- src/services/classes/listClassYear.service.ts
+++ src/services/classes/listClassYear.service.ts
@@ -14,7 +14,7 @@ export const listClassYearService = async ({
 
   let where_report = {}
 
-  if (is_report) where_report = { infrequencies: { some: { year_id } } }
+  if (is_report) where_report = { frequencies: { some: { year_id } } }
 
   const [data, total, dataLabel] = await Promise.all([
     prisma.classYear.findMany({
@@ -52,7 +52,6 @@ export const listClassYearService = async ({
       where: {
         school_id,
         year_id,
-        infrequencies: { some: { year_id } },
         ...where_report,
       },
       select: {
