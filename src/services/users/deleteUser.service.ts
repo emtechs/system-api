222dee2c0467710cd8ea5c6f3c02f19ace087f1d
Lucas Soares <lucassoares2007@hotmail.com>
2023-10-05 16:27:00 -0300

    feat: delete image

--- src/services/users/deleteUser.service.ts
+++ src/services/users/deleteUser.service.ts
@@ -1,11 +1,15 @@
 import { prisma } from '../../lib'
 import { AppError } from '../../errors'
+import { deleteImageKey } from '../../scripts'
 
 export const deleteUserService = async (login: string) => {
   try {
-    await prisma.user.delete({
+    const user = await prisma.user.delete({
       where: { login },
+      select: { profile: { select: { key: true } } },
     })
+
+    if (user.profile?.key) await deleteImageKey(user.profile.key)
   } catch {
     throw new AppError('user not found', 404)
   }
